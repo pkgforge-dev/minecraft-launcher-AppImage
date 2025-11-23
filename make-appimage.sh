8de4f003176370f426878daedd2802b9b46abfd4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-23 07:40:45 -0400

    fix locale issue

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/symbolic/apps/minecraft-launcher.svg
 export DESKTOP=/usr/share/applications/minecraft-launcher.desktop
+export LOCALE_FIX=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/minecraft-launcher*
